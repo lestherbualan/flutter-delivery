eab5057413e15f0030d6f1e2d56fe4848572785b
Lesther Bualan <lestherbualan1718@gmail.com>
2024-04-09 13:35:49 +0800

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import './authentication_screen/login_screen.dart';
 import './dashboard_screen/dashboard_screen.dart'; // Import the dashboard screen file
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
@@ -11,9 +17,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final providers = [EmailAuthProvider()];
+    return MaterialApp(
       title: 'Your App Title',
-      home: DashboardScreen(),
+      home: SignInScreen(
+        providers: providers,
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) {
+            Navigator.pushReplacementNamed(context, '/profile');
+          }),
+        ],
+      ),
     );
   }
 }
