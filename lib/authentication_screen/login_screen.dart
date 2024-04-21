b4635ab55a6a337d8ba9f6dffe30531704ca4433
Lesther Bualan <lestherbualan1718@gmail.com>
2024-04-22 04:59:50 +0800

    add asset auth logo and improve design in login and registration

--- lib/authentication_screen/login_screen.dart
+++ lib/authentication_screen/login_screen.dart
@@ -12,82 +12,115 @@ class LoginScreen extends StatelessWidget {
     String password = '';
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
+      backgroundColor: const Color(0xFFEDE1D5),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                onChanged: (value) {
-                  emailAddress = value;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Email',
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset(
+                  'assets/images/authlogo.jpeg', // Replace with your image path
+                  height: 250, // Adjust height as needed
+                  width: double.infinity,
+                  fit: BoxFit.fill,
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                onChanged: (value) {
-                  password = value;
-                },
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
+              Container(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextFormField(
+                      onChanged: (value) {
+                        emailAddress = value;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      onChanged: (value) {
+                        password = value;
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        try {
+                          final UserCredential userCredential =
+                              await FirebaseAuth.instance
+                                  .signInWithEmailAndPassword(
+                            email: emailAddress,
+                            password: password,
+                          );
+                          // Navigate to the dashboard screen upon successful login
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const DashboardScreen()),
+                          );
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'user-not-found') {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content:
+                                      Text('No user found for that email.')),
+                            );
+                          } else if (e.code == 'wrong-password') {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text(
+                                      'Wrong password provided for that user.')),
+                            );
+                          } else {
+                            print(e);
+                          }
+                        }
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextButton(
+                      onPressed: () {
+                        // Navigate to the registration screen
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const RegistrationScreen()),
+                        );
+                      },
+                      child: const Text('Don\'t have an Account? Sign up here'),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  final UserCredential userCredential =
-                      await FirebaseAuth.instance.signInWithEmailAndPassword(
-                    email: emailAddress,
-                    password: password,
-                  );
-                  // Navigate to the dashboard screen upon successful login
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const DashboardScreen()),
-                  );
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'user-not-found') {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content: Text('No user found for that email.')),
-                    );
-                  } else if (e.code == 'wrong-password') {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content:
-                              Text('Wrong password provided for that user.')),
-                    );
-                  } else {
-                    print(e);
-                  }
-                }
-              },
-              child: const Text('Login'),
-            ),
-            const SizedBox(height: 10),
-            OutlinedButton(
-              onPressed: () {
-                // Navigate to the registration screen
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const RegistrationScreen()),
-                );
-              },
-              child: const Text('Register'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/authentication_screen/registration_screen.dart
+++ lib/authentication_screen/registration_screen.dart
@@ -3,14 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../model/user.dart';
 
-class RegistrationScreen extends StatefulWidget {
+class RegistrationScreen extends StatelessWidget {
   const RegistrationScreen({Key? key}) : super(key: key);
 
   @override
-  _RegistrationScreenState createState() => _RegistrationScreenState();
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFFEDE1D5), // Background color set to #ede1d5
+      body: RegistrationForm(),
+    );
+  }
 }
 
-class _RegistrationScreenState extends State<RegistrationScreen> {
+class RegistrationForm extends StatefulWidget {
+  const RegistrationForm({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationFormState createState() => _RegistrationFormState();
+}
+
+class _RegistrationFormState extends State<RegistrationForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   String username = '';
@@ -28,149 +40,175 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Registration'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                onChanged: (value) {
-                  setState(() {
-                    username = value;
-                  });
-                },
-                decoration: InputDecoration(
-                  labelText: 'Username',
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Image.asset(
+                'assets/images/authlogo.jpeg', // Replace with your image path
+                height: 250, // Adjust height as needed
+                width: double.infinity,
+                fit: BoxFit.fill,
+              ),
+            ),
+            TextFormField(
+              onChanged: (value) {
+                setState(() {
+                  username = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Username',
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter a username';
-                  }
-                  return null;
-                },
               ),
-              const SizedBox(height: 20),
-              TextFormField(
-                onChanged: (value) {
-                  setState(() {
-                    emailAddress = value;
-                  });
-                },
-                decoration: InputDecoration(
-                  labelText: 'Email',
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a username';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              onChanged: (value) {
+                setState(() {
+                  emailAddress = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Email',
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter an email';
-                  }
-                  return null;
-                },
               ),
-              const SizedBox(height: 20),
-              TextFormField(
-                onChanged: (value) {
-                  setState(() {
-                    password = value;
-                  });
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter an email';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              onChanged: (value) {
+                setState(() {
+                  password = value;
+                });
+              },
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
               ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Are you a Rider?',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(width: 10),
-                  Switch(
-                    value: isRider,
-                    onChanged: (bool value) {
-                      setState(() {
-                        isRider = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    try {
-                      print('here!!!!');
-                      await FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                        email: emailAddress,
-                        password: password,
-                      )
-                          .then((value) {
-                        UserModel user =
-                            UserModel(uid: value.user!.uid, isRider: isRider);
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a password';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Are you a Rider?',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Switch(
+                  value: isRider,
+                  onChanged: (bool value) {
+                    setState(() {
+                      isRider = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  try {
+                    await FirebaseAuth.instance
+                        .createUserWithEmailAndPassword(
+                      email: emailAddress,
+                      password: password,
+                    )
+                        .then((value) {
+                      UserModel user =
+                          UserModel(uid: value.user!.uid, isRider: isRider);
 
-                        insertOrder(user).then((value) {
-                          print('registration complete');
-                        });
+                      insertOrder(user).then((value) {
+                        print('registration complete');
                       });
-                      // Registration successful, show pop-up
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text('Registration Complete'),
+                    });
+                    // Registration successful, show pop-up
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Registration Complete'),
+                          content:
+                              const Text('Your registration was successful!'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'weak-password') {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
                             content:
-                                const Text('Your registration was successful!'),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          );
-                        },
+                                Text('The password provided is too weak.')),
                       );
-                    } on FirebaseAuthException catch (e) {
-                      if (e.code == 'weak-password') {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                              content:
-                                  Text('The password provided is too weak.')),
-                        );
-                      } else if (e.code == 'email-already-in-use') {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                              content: Text(
-                                  'The account already exists for that email.')),
-                        );
-                      }
-                    } catch (e) {
-                      print(e);
+                    } else if (e.code == 'email-already-in-use') {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text(
+                                'The account already exists for that email.')),
+                      );
                     }
+                  } catch (e) {
+                    print(e);
                   }
-                },
-                child: const Text('Register'),
+                }
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                  ),
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
