cf602be0665741dd38716f5e7db0dc694b670826
Lesther Bualan <lestherbualan1718@gmail.com>
2024-12-08 23:51:01 +0800

    add restriction pop up on distance limit

--- lib/map_app/map_controller.dart
+++ lib/map_app/map_controller.dart
@@ -15,6 +15,8 @@ class MapTapController {
   final List<GeoPoint> pointsRoad = [];
   final Geodesy geodesy = Geodesy();
 
+  BuildContext? _context;
+
   MapTapController(this.controller,
       {this.updateDistance,
       this.updateStartingAddress,
@@ -24,13 +26,17 @@ class MapTapController {
     controller.listenerMapSingleTapping.addListener(_handleMapTap);
   }
 
+  // Method to set the context
+  void setContext(BuildContext context) {
+    _context = context;
+  }
+
   void _handleMapTap() async {
     if (controller.listenerMapSingleTapping.value != null) {
       GeoPoint tappedPoint = controller.listenerMapSingleTapping.value!;
       if (pointsRoad.isEmpty) {
         pointsRoad.add(tappedPoint);
-        getCoordinateInfo(pointsRoad[0].latitude, pointsRoad[0].longitude)
-            .then((value) {
+        getCoordinateInfo(pointsRoad[0].latitude, pointsRoad[0].longitude).then((value) {
           updateStartingAddress?.call(value);
           updateStartingGeopoint?.call(tappedPoint);
         });
@@ -45,35 +51,59 @@ class MapTapController {
           ),
         );
       } else {
-        pointsRoad.add(tappedPoint);
-        getCoordinateInfo(pointsRoad[1].latitude, pointsRoad[1].longitude)
-            .then((value) {
-          updateEndingAddress?.call(value);
-          updateEndingGeopoint?.call(tappedPoint);
-        });
-        await controller.addMarker(
-          tappedPoint,
-          markerIcon: const MarkerIcon(
-            icon: Icon(
-              Icons.person_pin_circle,
-              color: Colors.blueAccent,
-              size: 48,
-            ),
-          ),
-        );
-        controller.drawRoad(
-          pointsRoad.first,
-          pointsRoad.last,
-          roadType: RoadType.bike,
-          intersectPoint: pointsRoad.sublist(1),
-          roadOption: const RoadOption(
-            roadColor: Colors.blue,
-            roadWidth: 10,
-            zoomInto: true,
-          ),
-        );
-        double distance = _calculateDistance();
-        updateDistance?.call(distance);
+        if (pointsRoad.length <= 1) {
+          pointsRoad.add(tappedPoint);
+
+          double firstDistance = _calculateDistance();
+          if (!(firstDistance < 1.0)) {
+            getCoordinateInfo(pointsRoad[1].latitude, pointsRoad[1].longitude).then((value) {
+              updateEndingAddress?.call(value);
+              updateEndingGeopoint?.call(tappedPoint);
+            });
+            await controller.addMarker(
+              tappedPoint,
+              markerIcon: const MarkerIcon(
+                icon: Icon(
+                  Icons.person_pin_circle,
+                  color: Colors.blueAccent,
+                  size: 48,
+                ),
+              ),
+            );
+            controller.drawRoad(
+              pointsRoad.first,
+              pointsRoad.last,
+              roadType: RoadType.bike,
+              intersectPoint: pointsRoad.sublist(1),
+              roadOption: const RoadOption(
+                roadColor: Colors.blue,
+                roadWidth: 10,
+                zoomInto: true,
+              ),
+            );
+            double distance = _calculateDistance();
+            updateDistance?.call(distance);
+          } else {
+            pointsRoad.removeLast();
+            showDialog(
+              context: _context!,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Invalid Point'),
+                  content: const Text('The points are too close to each other. Please select a point further away.'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(); // Close the dialog
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+        }
       }
     }
   }
@@ -83,8 +113,7 @@ class MapTapController {
     GeoPoint end = pointsRoad[pointsRoad.length - 1];
     LatLng startLatLng = LatLng(start.latitude, start.longitude);
     LatLng endLatLng = LatLng(end.latitude, end.longitude);
-    double distance =
-        geodesy.distanceBetweenTwoGeoPoints(startLatLng, endLatLng) as double;
+    double distance = geodesy.distanceBetweenTwoGeoPoints(startLatLng, endLatLng) as double;
     return distance / 1000;
   }
 
@@ -97,8 +126,7 @@ class MapTapController {
       GeocodingPlatform? geocodingPlatform = GeocodingPlatform.instance;
 
       // Get placemarks using the new instance
-      List<Placemark> placemarks = await geocodingPlatform!
-          .placemarkFromCoordinates(latitude, longitude);
+      List<Placemark> placemarks = await geocodingPlatform!.placemarkFromCoordinates(latitude, longitude);
 
       Placemark firstPlacemark = placemarks.first;
       street = firstPlacemark.street ?? '';
