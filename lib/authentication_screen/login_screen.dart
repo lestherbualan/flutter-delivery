a5b95f85a44b3b48d32ffffdf9f388007e14fb52
Lesther Bualan <lestherbualan1718@gmail.com>
2024-12-09 14:45:17 +0800

    fix pop up error credentials on login page

--- lib/authentication_screen/login_screen.dart
+++ lib/authentication_screen/login_screen.dart
@@ -4,109 +4,118 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../dashboard_screen/dashboard_screen.dart';
 import 'registration_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    String emailAddress = '';
-    String password = '';
-    TextEditingController email = TextEditingController(text: "driver@gmail.com");
-    TextEditingController pass = TextEditingController(text: "driver");
-
-    final TextEditingController _emailController = TextEditingController();
-    final TextEditingController _passwordController = TextEditingController();
-
     return Scaffold(
       backgroundColor: const Color(0xFFEDE1D5),
-      body: Builder(builder: (context) {
-        return Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Image.asset(
-                    'assets/images/authlogo.jpg', // Replace with your image path
-                    height: 250, // Adjust height as needed
-                    width: double.infinity,
-                    fit: BoxFit.fill,
-                  ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset(
+                  'assets/images/authlogo.jpg', // Replace with your image path
+                  height: 250, // Adjust height as needed
+                  width: double.infinity,
+                  fit: BoxFit.fill,
                 ),
-                Container(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      LoginInputField(
-                        labelText: 'Email',
-                        hintText: 'Please enter your email',
-                        icon: Icons.email_outlined,
-                        controller: _emailController,
-                      ),
-                      LoginInputField(
-                        labelText: 'Password',
-                        hintText: 'Please enter your password',
-                        icon: Icons.lock_outline,
-                        obscureText: true,
-                        controller: _passwordController,
-                      ),
-                      SizedBox(height: 20),
-                      OutlinedButton(
-                        style: OutlinedButton.styleFrom(backgroundColor: Colors.white70),
-                        onPressed: () async {
-                          FocusScope.of(context).unfocus();
-                          try {
-                            final UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-                              email: _emailController.value.text,
-                              password: _passwordController.value.text,
-                            );
-                            print(_emailController.value.text);
-                            print(_passwordController.value.text);
-                          } on FirebaseAuthException catch (e) {
-                            if (e.code == 'user-not-found') {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(content: Text('No user found for that email.')),
-                              );
-                            } else if (e.code == 'wrong-password') {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(content: Text('Wrong password provided for that user.')),
-                              );
-                            } else {
-                              print(e);
-                            }
+              ),
+              Container(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    LoginInputField(
+                      labelText: 'Email',
+                      hintText: 'Please enter your email',
+                      icon: Icons.email_outlined,
+                      controller: _emailController,
+                    ),
+                    LoginInputField(
+                      labelText: 'Password',
+                      hintText: 'Please enter your password',
+                      icon: Icons.lock_outline,
+                      obscureText: true,
+                      controller: _passwordController,
+                    ),
+                    const SizedBox(height: 20),
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(backgroundColor: Colors.white70),
+                      onPressed: () async {
+                        FocusScope.of(context).unfocus();
+                        try {
+                          final UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                            email: _emailController.text.trim(),
+                            password: _passwordController.text.trim(),
+                          );
+
+                          // Navigate to DashboardScreen upon successful login
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const DashboardScreen()),
+                          );
+                        } on FirebaseAuthException catch (e) {
+                          String message;
+                          if (e.code == 'user-not-found') {
+                            message = 'No user found for that email.';
+                          } else if (e.code == 'wrong-password') {
+                            message = 'Wrong password provided for that user.';
+                          } else {
+                            message = 'An unexpected error occurred: ${e.message}';
                           }
-                        },
-                        child: const Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Text(
-                            'Sign In',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18.0,
-                            ),
+
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(message)),
+                          );
+                        }
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
                           ),
                         ),
                       ),
-                      SizedBox(height: 10),
-                      TextButton(
-                        onPressed: () {
-                          // Navigate to the registration screen
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const RegistrationScreen()),
-                          );
-                        },
-                        child: const Text('Don\'t have an Account? Sign up here'),
-                      ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const RegistrationScreen()),
+                        );
+                      },
+                      child: const Text('Don\'t have an Account? Sign up here'),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        );
-      }),
+        ),
+      ),
     );
   }
 }
